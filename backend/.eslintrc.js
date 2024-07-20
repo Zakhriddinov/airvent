97a8389543f1b8ded0b70d049512cc8157d749f6
Sardor <zaxriddinovsardor21@gmail.com>
2024-07-20 07:35:54 +0500

    add

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'script', // sourceType ni 'script' ga o'zgartiring
   },
   rules: {
     'no-console': 0,
