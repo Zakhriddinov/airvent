16ddf24ae4ab4bd84cf2a22efc4ab66b1a2dd1a3
Sardor <zaxriddinovsardor21@gmail.com>
2024-07-21 22:02:38 +0500

    add

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 const connectDB = require('./config/database');
 const logger = require('./config/logger');
 require('colors');
-const http = require('http');
-const express = require('express');
 
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 20) {
@@ -14,39 +12,9 @@ require('dotenv').config({ path: '.env' });
 // connect database
 connectDB();
 
-// Create Express app
-const app = express();
-
-const checkPort = (port) => {
-  return new Promise((resolve, reject) => {
-    const server = http.createServer(app);
-    server.listen(port, () => {
-      server.close(() => resolve(port));
-    });
-    server.on('error', () => {
-      reject(port);
-    });
-  });
-};
-
-const getAvailablePort = async (startPort) => {
-  let port = startPort;
-  while (true) {
-    try {
-      await checkPort(port);
-      return port;
-    } catch (err) {
-      port++;
-    }
-  }
-};
-
-const startServer = async () => {
-  const port = await getAvailablePort(process.env.PORT || 8888);
-  app.set('port', port);
-  const server = app.listen(port, () => {
-    logger.info(`Express running → On PORT : ${server.address().port}`.blue);
-  });
-};
-
-startServer();
+// Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 8888);
+const server = app.listen(app.get('port'), () => {
+  logger.info(`Express running → On PORT : ${server.address().port}`.blue);
+});
